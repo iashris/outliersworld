53a1e3fb5e982c797af2b0f2117f07d22e8ed01d
Ashris Choudhury <ashris.me@gmail.com>
2017-09-28 14:32:12 -0400

    push to github

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ var router = express.Router();
 var path=require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var viewOpts = { root: path.join(__dirname,'welcome') };
-  res.sendFile('index.html',viewOpts);
+  var viewOpts = { root: __dirname };
+  res.sendFile('index.html');
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('index', { layout: 'layout.hbs' })
 });
 
 module.exports = router;
