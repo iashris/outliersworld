e6c883bbb0fdfeac9d6df59b9e449d718f6553fc
Ashris Choudhury <ashris.me@gmail.com>
2017-10-25 02:59:22 -0400

    projects backend

--- routes/index.js
+++ routes/index.js
@@ -13,29 +13,34 @@ router.get('/home',function(req,res,next){
 router.get('/projects', function(req, res, next) {
 	//Load Outliers Projects DB and send all projects for projects to render
 	gsheet('1sFlWBjlFLCOXvl-PixptFTBwAj-2uUNF4A8oMBxmrds', (data)=>{
-	 var DATA={};
-	 //res.render('projects', { layout: 'layout.hbs' ,'data':DATA})
-	 res.send(data)
+		console.log(data);
+	 res.render('projects', { layout: 'wlayout.hbs' ,'data':data})
 	});
 });
-
+router.get('/project/:projectid',function(req,res){
+		gsheet('1sFlWBjlFLCOXvl-PixptFTBwAj-2uUNF4A8oMBxmrds', (data)=>{
+		//load correct data
+		//get likes for project
+	 res.render('projects', { layout: 'wlayout.hbs' ,'data':data})
+	});
+});
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { layout: 'layout.hbs' })
+  res.render('contact', { layout: 'wlayout.hbs' })
 });
 router.get('/clusters', function(req, res, next) {
   	gsheet('1XrDALyA84t4jT3Mh2WdhAd_lzpyc9eSfT_n5MoV1bOA', (data)=>{
 	 var DATA={};
-	 res.render('clusters', { layout: 'layout.hbs' ,'data':DATA})
+	 res.render('clusters', { layout: 'wlayout.hbs' ,'data':DATA})
 	});
 });
 router.get('/resources', function(req, res, next) {
-	gsheet('1ZEyivQwtoQwNdXLCSkGXtlOxUvAV2kbXYhA1XLzMJcQ/edit#gid=226239407', (data)=>{
+	//gsheet('1ZEyivQwtoQwNdXLCSkGXtlOxUvAV2kbXYhA1XLzMJcQ/edit#gid=226239407', (data)=>{
 
-  console.log('Try callback ', data.length);  // array of objects
- // res.render('resources', { layout: 'layout.hbs' })
- res.send(data);
+  //console.log('Try callback ', data.length);  // array of objects
+ res.render('resources', { layout: 'wlayout.hbs' })
+ //res.send(data);
 
-});
+//});
 
 });
 
